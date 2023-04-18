7b051a4acee56121898baa4b487c16427d63f743
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-04-19 01:10:20 +0900

    feat: add scripts

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# start ec2
-
-SSH_CMD="ssh personal_ubuntu"
+SSH_CMD="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null personal_ubuntu"
 
 $SSH_CMD 'bash -c "sudo apt update && sudo apt install -y gcc make git binutils libc6-dev"'
 
@@ -10,5 +8,3 @@ $SSH_CMD 'bash -c "mkdir -p ./wspace"'
 $SSH_CMD 'bash -c "cd wspace && git clone https://github.com/rui314/chibicc.git || git pull"'
 $SSH_CMD 'bash -c "cd wspace && git clone https://github.com/miyataka/compilerbook-lesson.git || git pull"'
 $SSH_CMD
-
-# stop ec2
