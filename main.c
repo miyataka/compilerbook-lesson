4552de6c501dbd6ea476132c3d0026531dcb1444
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-07-12 12:19:46 +0900

    feat: unary()

--- main.c
+++ main.c
@@ -157,6 +157,7 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -173,13 +174,13 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_binary(ND_MUL, node, primary());
+      node = new_binary(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_binary(ND_DIV, node, primary());
+      node = new_binary(ND_DIV, node, unary());
     else
       return node;
   }
@@ -195,6 +196,14 @@ Node *primary() {
 
   // そうでなければ数値のはず
   return new_num(expect_number());
+}
+
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
 }
 
 void gen(Node *node) {
